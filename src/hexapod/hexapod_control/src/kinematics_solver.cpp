98b74881c3af9a2fde05694121bec66937210373
Kyle Erf <kyle.w.erf@gmail.com>
2021-08-18 00:35:26 -0400

    Added kinematics solver and joint/pose/trajectory action servers for L1

---
+++ src/hexapod/hexapod_control/src/kinematics_solver.cpp
@@ -0,0 +1,289 @@
+#include <ros/ros.h>
+#include "kdl/chainiksolverpos_lma.hpp"
+#include "kdl/chainiksolvervel_pinv.hpp"
+#include "kdl/chainfksolverpos_recursive.hpp"
+#include "kdl_parser/kdl_parser.hpp"
+#include "hexapod_control/SolveIKPose.h"
+#include "hexapod_control/SolveFKPose.h"
+#include "hexapod_control/Pose.h"
+
+namespace hexapod_control
+{
+    class KinematicsSolver
+    {
+    public:
+        KinematicsSolver(ros::NodeHandle* node, KDL::Tree tree)
+        {
+            this->node = node;
+
+            KDL::Chain chain;
+            tree.getChain("body", "foot_L1", this->leg_L1);
+            tree.getChain("body", "foot_R1", this->leg_R1);
+            tree.getChain("body", "foot_L2", this->leg_L2);
+            tree.getChain("body", "foot_R2", this->leg_R2);
+            tree.getChain("body", "foot_L3", this->leg_L3);
+            tree.getChain("body", "foot_R3", this->leg_R3);
+
+            ROS_INFO("Initializing IKPoseSolver service...");
+            this->ikL1PoseService = node->advertiseService("/hexapod/leg_L1/ik", &KinematicsSolver::solveIKPoseLegL1, this);
+            this->ikR1PoseService = node->advertiseService("/hexapod/leg_R1/ik", &KinematicsSolver::solveIKPoseLegR1, this);
+            this->ikL2PoseService = node->advertiseService("/hexapod/leg_L2/ik", &KinematicsSolver::solveIKPoseLegL2, this);
+            this->ikR2PoseService = node->advertiseService("/hexapod/leg_R2/ik", &KinematicsSolver::solveIKPoseLegR2, this);
+            this->ikL3PoseService = node->advertiseService("/hexapod/leg_L3/ik", &KinematicsSolver::solveIKPoseLegL3, this);
+            this->ikR3PoseService = node->advertiseService("/hexapod/leg_R3/ik", &KinematicsSolver::solveIKPoseLegR3, this);
+            
+            ROS_INFO("Initializing FKPoseSolver service...");
+            this->fkL1PoseService = node->advertiseService("/hexapod/leg_L1/fk", &KinematicsSolver::solveFKPoseLegL1, this);
+            this->fkR1PoseService = node->advertiseService("/hexapod/leg_R1/fk", &KinematicsSolver::solveFKPoseLegR1, this);
+            this->fkL2PoseService = node->advertiseService("/hexapod/leg_L2/fk", &KinematicsSolver::solveFKPoseLegL2, this);
+            this->fkR2PoseService = node->advertiseService("/hexapod/leg_R2/fk", &KinematicsSolver::solveFKPoseLegR2, this);
+            this->fkL3PoseService = node->advertiseService("/hexapod/leg_L3/fk", &KinematicsSolver::solveFKPoseLegL3, this);
+            this->fkR3PoseService = node->advertiseService("/hexapod/leg_R3/fk", &KinematicsSolver::solveFKPoseLegR3, this);
+
+            ROS_INFO("Ready.");
+        }
+
+        ~KinematicsSolver()
+        {
+            this->node->shutdown();
+        }
+
+        bool solveIKPose(hexapod_control::SolveIKPoseRequest& req,
+                         hexapod_control::SolveIKPoseResponse& res,
+                         KDL::Chain chain)
+        {
+            ROS_DEBUG("Solving IK Pose...");
+            ROS_DEBUG("Initializing solver...");
+
+            Eigen::Matrix<double, 6, 1> weights;
+            weights.col(0) << 1.0, 1.0, 1.0, 0.0, 0.0, 0.0;
+
+            KDL::ChainIkSolverPos_LMA posSolver(chain, weights);
+            std::vector<double> initialstate = req.initialState;
+
+            KDL::JntArray jntArray = KDL::JntArray(3);
+            jntArray.data = Eigen::Map<const Eigen::VectorXd>(initialstate.data(), initialstate.size());
+
+            ROS_DEBUG("Parsing rotation...");
+            KDL::Vector rotx = KDL::Vector(req.goal.rotx[0], req.goal.rotx[1], req.goal.rotx[2]);
+            KDL::Vector roty = KDL::Vector(req.goal.roty[0], req.goal.roty[1], req.goal.roty[2]);
+            KDL::Vector rotz = KDL::Vector(req.goal.rotz[0], req.goal.rotz[1], req.goal.rotz[2]);
+
+            ROS_DEBUG("Parsing posiion...");
+            KDL::Vector targetpos = KDL::Vector(req.goal.x, req.goal.y, req.goal.z);
+            KDL::Rotation targetrot = KDL::Rotation(rotx, roty, rotz);
+
+            ROS_DEBUG("Generating goal...");
+            KDL::Frame targetFrame = KDL::Frame(targetrot, targetpos);
+
+            KDL::JntArray jntSol = KDL::JntArray(3);
+            ROS_DEBUG("[%f, %f, %f]; [[%f, %f, %f], [%f, %f, %f], [%f, %f, %f]]",
+                        targetpos.data[0], targetpos.data[1], targetpos.data[2],
+                        rotx.data[0], roty.data[0], rotz.data[0],
+                        rotx.data[1], roty.data[1], rotz.data[1],
+                        rotx.data[2], roty.data[2], rotz.data[2]);
+
+            ROS_DEBUG("Running solver...");
+            int result = posSolver.CartToJnt(jntArray, targetFrame, jntSol);
+            ROS_DEBUG("Result: %d", result);
+
+            for (int i = 0; i < jntSol.rows() * jntSol.columns(); i++)
+            {
+                ROS_DEBUG("%f", jntSol.data[i]);
+            }
+
+            std::vector<double> sol(jntSol.data.data(), jntSol.data.data() + jntSol.data.rows() * jntSol.data.cols());
+
+            res.solution = sol;
+            res.result = result;
+            res.error_message = std::string(posSolver.strError(res.result));
+            return true;
+        }
+
+        bool solveFKPose(hexapod_control::SolveFKPoseRequest &req,
+                         hexapod_control::SolveFKPoseResponse &res,
+                         KDL::Chain chain)
+        {
+            ROS_DEBUG("FK Pose request received.");
+            KDL::ChainFkSolverPos_recursive posSolver(chain);
+
+            ROS_DEBUG("Joint positions:");
+            std::vector<double> jointPositions = req.jointPositions;
+            for (int i = 0; i < jointPositions.size(); ++i)
+            {
+                ROS_DEBUG("  %f", jointPositions[i]);
+            }
+
+            KDL::JntArray jntArray = KDL::JntArray(7);
+            jntArray.data = Eigen::Map<const Eigen::VectorXd>(jointPositions.data(), jointPositions.size());
+
+            KDL::Frame frame = KDL::Frame();
+
+            int result = posSolver.JntToCart(jntArray, frame);
+            res.result = result;
+            res.error_message = std::string(posSolver.strError(res.result));
+
+            res.solution.x = frame.p.data[0];
+            res.solution.y = frame.p.data[1];
+            res.solution.z = frame.p.data[2];
+
+            std::vector<double> rotx = std::vector<double> {
+                frame.M.UnitX().data[0],
+                frame.M.UnitX().data[1],
+                frame.M.UnitX().data[2]
+            };
+            
+            std::vector<double> roty = std::vector<double> {
+                frame.M.UnitY().data[0],
+                frame.M.UnitY().data[1],
+                frame.M.UnitY().data[2]
+            };
+
+            std::vector<double> rotz = std::vector<double> {
+                frame.M.UnitZ().data[0],
+                frame.M.UnitZ().data[1],
+                frame.M.UnitZ().data[2]
+            };
+
+            res.solution.rotx = rotx;
+            res.solution.roty = roty;
+            res.solution.rotz = rotz;
+
+            ROS_DEBUG("Solved position: {%f, %f, %f}", res.solution.x, res.solution.y, res.solution.z);
+            ROS_DEBUG("Solved rotx: {%f, %f, %f}", rotx[0], rotx[1], rotx[2]);
+            ROS_DEBUG("Solved roty: {%f, %f, %f}", roty[0], roty[1], roty[2]);
+            ROS_DEBUG("Solved rotz: {%f, %f, %f}", rotz[0], rotz[1], rotz[2]);
+
+            return true;
+        }
+
+        bool solveIKPoseLegL1(hexapod_control::SolveIKPoseRequest &req,
+                              hexapod_control::SolveIKPoseResponse &res)
+        {
+            return solveIKPose(req, res, this->leg_L1);
+        }
+
+        bool solveIKPoseLegR1(hexapod_control::SolveIKPoseRequest &req,
+                              hexapod_control::SolveIKPoseResponse &res)
+        {
+            return solveIKPose(req, res, this->leg_R1);
+        }
+
+        bool solveIKPoseLegL2(hexapod_control::SolveIKPoseRequest &req,
+                              hexapod_control::SolveIKPoseResponse &res)
+        {
+            return solveIKPose(req, res, this->leg_L2);
+        }
+
+        bool solveIKPoseLegR2(hexapod_control::SolveIKPoseRequest &req,
+                              hexapod_control::SolveIKPoseResponse &res)
+        {
+            return solveIKPose(req, res, this->leg_R2);
+        }
+
+        bool solveIKPoseLegL3(hexapod_control::SolveIKPoseRequest &req,
+                              hexapod_control::SolveIKPoseResponse &res)
+        {
+            return solveIKPose(req, res, this->leg_L3);
+        }
+
+        bool solveIKPoseLegR3(hexapod_control::SolveIKPoseRequest &req,
+                              hexapod_control::SolveIKPoseResponse &res)
+        {
+            return solveIKPose(req, res, this->leg_R3);
+        }
+
+        bool solveFKPoseLegL1(hexapod_control::SolveFKPoseRequest &req,
+                              hexapod_control::SolveFKPoseResponse &res)
+        {
+            return solveFKPose(req, res, this->leg_L1);
+        }
+
+        bool solveFKPoseLegR1(hexapod_control::SolveFKPoseRequest &req,
+                              hexapod_control::SolveFKPoseResponse &res)
+        {
+            return solveFKPose(req, res, this->leg_R1);
+        }
+
+        bool solveFKPoseLegL2(hexapod_control::SolveFKPoseRequest &req,
+                              hexapod_control::SolveFKPoseResponse &res)
+        {
+            return solveFKPose(req, res, this->leg_L2);
+        }
+
+        bool solveFKPoseLegR2(hexapod_control::SolveFKPoseRequest &req,
+                              hexapod_control::SolveFKPoseResponse &res)
+        {
+            return solveFKPose(req, res, this->leg_R2);
+        }
+
+        bool solveFKPoseLegL3(hexapod_control::SolveFKPoseRequest &req,
+                              hexapod_control::SolveFKPoseResponse &res)
+        {
+            return solveFKPose(req, res, this->leg_L3);
+        }
+
+        bool solveFKPoseLegR3(hexapod_control::SolveFKPoseRequest &req,
+                              hexapod_control::SolveFKPoseResponse &res)
+        {
+            return solveFKPose(req, res, this->leg_R3);
+        }
+
+    private:
+        ros::NodeHandle* node;
+        ros::ServiceServer ikL1PoseService;
+        ros::ServiceServer ikR1PoseService;
+        ros::ServiceServer ikL2PoseService;
+        ros::ServiceServer ikR2PoseService;
+        ros::ServiceServer ikL3PoseService;
+        ros::ServiceServer ikR3PoseService;
+        ros::ServiceServer fkL1PoseService;
+        ros::ServiceServer fkR1PoseService;
+        ros::ServiceServer fkL2PoseService;
+        ros::ServiceServer fkR2PoseService;
+        ros::ServiceServer fkL3PoseService;
+        ros::ServiceServer fkR3PoseService;
+        KDL::Chain leg_L1;
+        KDL::Chain leg_R1;
+        KDL::Chain leg_L2;
+        KDL::Chain leg_R2;
+        KDL::Chain leg_L3;
+        KDL::Chain leg_R3;
+    };
+} // namespace hexapod_control
+
+int main(int argc, char **argv)
+{
+    ROS_INFO("Starting Inverse Kinematics solver...");
+    ROS_INFO("Building robot tree from param server...");
+    ros::init(argc, argv, "kinematics_solver");
+    ros::NodeHandle node;
+    std::string robot_desc_string;
+    node.param("/robot_description", robot_desc_string, std::string());
+    KDL::Tree tree;
+
+    if (!kdl_parser::treeFromString(robot_desc_string, tree))
+    {
+        ROS_INFO("Failed to construct kdl tree");
+        return 0;
+    }
+    else
+    {
+        ROS_INFO("Build chain of %d segments", tree.getNrOfSegments());
+    }
+
+    KDL::SegmentMap::const_iterator it;
+    for (it = tree.getSegments().begin(); it != tree.getSegments().end(); it++)
+    {
+        ROS_INFO("Segment %s", it->second.segment.getName().c_str());
+    }
+
+    hexapod_control::KinematicsSolver solver(&node, tree);
+
+    //ros::spin();
+    ros::AsyncSpinner spinner(4);
+    spinner.start();
+    ros::waitForShutdown();
+
+    return 0;
+}
---
+++ src/hexapod/hexapod_control/src/leg_L1_trajectory_action_server.cpp
@@ -0,0 +1,264 @@
+#include "actionlib/server/simple_action_server.h"
+#include "actionlib/client/simple_action_client.h"
+#include "hexapod_control/SetPoseAction.h"
+#include "hexapod_control/GaitAction.h"
+#include "hexapod_control/Pose.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Bool.h"
+#include "geometry_msgs/Pose.h"
+#include "gazebo_msgs/GetLinkState.h"
+#include "math.h"
+
+class SetTrajectoryAction
+{
+public:
+    SetTrajectoryAction(std::string name):
+        server(node, name, boost::bind(&SetTrajectoryAction::executeCB, this, _1), false),
+        client("leg_L1_pose_action", true),
+        actionName(name)
+    {
+        this->node = node;
+
+        ROS_INFO("Waiting for Pose State Server...");
+        this->client.waitForServer(ros::Duration(30));
+
+		ROS_INFO("Subscribing to Gait Controller...");
+		this->dutyFactorSubscriber = node.subscribe("/hexapod/gait/duty_factor", 10, &SetTrajectoryAction::dutyFactorCB, this);
+		this->bodyVelocitySubscriber = node.subscribe("/hexapod/gait/body_velocity", 10, &SetTrajectoryAction::bodyVelocityCB, this);
+        this->stopCommandSubscriber = node.subscribe("/hexapod/gait/stop", 10, &SetTrajectoryAction::stopCommandCB, this);
+
+        ROS_INFO("Subscribing to Gazebo GetLinkState service");
+        this->linkStateClient = node.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
+		
+		ROS_INFO("Starting...");
+		server.start();
+    }
+
+	~SetTrajectoryAction()
+	{
+		this->node.shutdown();
+	}
+
+	void executeCB(const hexapod_control::GaitGoalConstPtr &goal)
+	{
+		double start = ros::Time::now().toSec();
+
+		this->initialPhase = goal->initialPhase;
+		this->strideTime = goal->strideTime;
+		this->strideHeight = goal->strideHeight;
+		double eps = 0.05;
+		
+		int state = 0;
+        bool preempted = false;
+        stop = false;
+        initialized = false;
+
+		double t, elapsed;
+		steps = 0;
+		currentPhase = this->initialPhase;
+		hexapod_control::Pose targetPose;
+
+		ros::Rate rate(50);
+		while (true)
+		{
+            // Check if preempted or canceled
+            if (server.isPreemptRequested() || !ros::ok())
+            {
+                ROS_INFO("Trajectory action preempted, ending trajectory...");
+                preempted = true;
+            }
+
+            elapsed = ros::Time::now().toSec() - start;
+			t = elapsed + this->initialPhase * strideTime; // account for initialPhase
+
+            // Calculate the current phase
+			currentPhase = fmod(t / this->strideTime, 1.0);
+            if (!initialized && currentPhase >= dutyFactor)
+            {
+                initialized = true;
+            }
+
+            // Calculate the target position
+            geometry_msgs::Point position;
+            if (!initialized)
+            {
+                position = InitialTrajectory(elapsed);
+            }
+            else
+            {
+                position = SineTrajectory(currentPhase);
+            }
+
+            if ((stop || preempted) && stage.compare("Support Phase") == 0)
+            {
+                break;
+            }
+
+			// Build message
+			targetPose.x = position.x;
+			targetPose.y = position.y;
+			targetPose.z = position.z;
+			targetPose.rotx = std::vector<double>{1.0, 0.0, 0.0};
+			targetPose.roty = std::vector<double>{0.0, 1.0, 0.0};
+			targetPose.rotz = std::vector<double>{0.0, 0.0, 1.0};
+
+			// Send parameters to pose action client
+			hexapod_control::SetPoseGoal poseAction;
+			poseAction.goal = targetPose;
+			poseAction.eps = eps;
+			this->client.sendGoal(poseAction,
+				boost::bind(&SetTrajectoryAction::publishResult, this, _1, _2),
+				boost::bind(&SetTrajectoryAction::activeCB, this),
+				boost::bind(&SetTrajectoryAction::publishFeedback, this, _1));
+
+			this->actionFeedback.currentPhase = currentPhase;
+			this->actionFeedback.targetPose = targetPose;
+			this->actionFeedback.currentPose = currentPose;
+            this->actionFeedback.stage = stage;
+			server.publishFeedback(this->actionFeedback);
+
+            if (currentPhase == initialPhase)
+            {
+                steps++;
+            }
+
+			rate.sleep();
+		}
+
+		// Publish result
+        this->actionResult.stepsTaken = steps;
+        if (preempted)
+        {
+            server.setPreempted(this->actionResult);
+        }
+        else
+        {
+		    server.setSucceeded(this->actionResult);
+        }
+	}
+
+	void publishFeedback(const hexapod_control::SetPoseFeedback::ConstPtr& poseFeedback)
+	{
+		currentPose = poseFeedback->currentPose;
+	}
+
+	void publishResult(const actionlib::SimpleClientGoalState& state,
+		const hexapod_control::SetPoseResult::ConstPtr& poseResult)
+	{
+
+	}
+
+	void activeCB()
+	{
+
+	}
+
+	void dutyFactorCB(const std_msgs::Float64ConstPtr& msg)
+	{
+		this->dutyFactor = msg->data;
+	}
+
+	void bodyVelocityCB(const std_msgs::Float64ConstPtr& msg)
+	{
+		this->bodyVelocity = msg->data;
+	}
+
+    void stopCommandCB(const std_msgs::BoolConstPtr& msg)
+	{
+		this->stop = msg->data;
+	}
+
+private:
+	std::string actionName;
+	ros::NodeHandle node;
+	actionlib::SimpleActionServer<hexapod_control::GaitAction> server;
+	actionlib::SimpleActionClient<hexapod_control::SetPoseAction> client;
+    ros::ServiceClient linkStateClient;
+	hexapod_control::GaitFeedback actionFeedback;
+	hexapod_control::GaitResult actionResult;
+	ros::Subscriber dutyFactorSubscriber;
+	ros::Subscriber bodyVelocitySubscriber;
+    ros::Subscriber stopCommandSubscriber;
+	hexapod_control::Pose currentPose;
+	double initialPhase;
+	double currentPhase;
+	double strideTime;
+	double strideHeight;
+	double dutyFactor;
+	double bodyVelocity;
+    double xOffset = -0.1695;
+    double yOffset = 0.1524;
+    double zOffset = -0.2855;
+    // double xOffset = -0.2;
+    // double yOffset = 0.1524;
+    // double zOffset = -0.275;
+    int steps = 0;
+    bool stop = false;
+    bool initialized = false;
+    std::string stage;
+
+    geometry_msgs::Point InitialTrajectory(double elapsed)
+    {
+        // Position w.r.t. body
+        geometry_msgs::Point position;
+        double x, y, z;
+
+        // Just move forward until reach initial phase
+        x = xOffset - bodyVelocity * elapsed;
+        y = yOffset;
+        z = zOffset;
+        stage = "Initialization";
+
+        position.x = x;
+        position.y = y;
+        position.z = z;
+
+        return position;
+    }
+
+    geometry_msgs::Point SineTrajectory(double phase)
+    {
+        // Position w.r.t. body
+        geometry_msgs::Point position;
+        double x, y, z;
+
+        double strideLength = strideTime * bodyVelocity;
+
+        // Support phase
+        if (phase < dutyFactor)
+        {
+            double supportPhase = phase / dutyFactor;
+            x = x = xOffset + (strideLength / 2 - strideLength * supportPhase);
+            y = yOffset;
+            z = zOffset;
+            stage = "Support Phase";
+        }
+        // Transfer phase
+        else
+        {
+            double transferPhase = (phase - dutyFactor) / (1.0 - dutyFactor);
+            x = xOffset + (strideLength * transferPhase - strideLength / 2);
+            y = yOffset;
+            z = 0.5 * strideHeight * cos(2 * M_PI * transferPhase - M_PI) + zOffset + strideHeight/2;
+            stage = "Transfer Phase";
+        }
+        
+        position.x = x;
+        position.y = y;
+        position.z = z;
+
+        return position;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    ROS_INFO("Starting Trajectory Action Server...");
+    ros::init(argc, argv, "leg_L1_trajectory_action");
+    ROS_INFO("Initialized ros...");
+
+    SetTrajectoryAction actionServer("leg_L1_trajectory_action");
+    ROS_INFO("Spinning node...");
+    ros::spin();
+    return 0;
+}
