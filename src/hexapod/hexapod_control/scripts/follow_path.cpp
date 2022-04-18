8bfb5da6d0c2fc16bf044812ada11a4efe663d32
Kyle Erf <kyle.w.erf@gmail.com>
2022-04-18 00:50:13 -0400

    Added code to generator graph and solve A* search algorithm

--- src/hexapod/hexapod_control/scripts/follow_path.cpp
+++ src/hexapod/hexapod_control/scripts/follow_path.cpp
@@ -5,6 +5,8 @@
 #include "boost/thread/mutex.hpp"
 #include "boost/thread/thread.hpp"
 #include "gazebo_msgs/GetLinkState.h"
+#include <queue>
+#include <unordered_map>
 
 class FollowPath
 {
@@ -12,6 +14,7 @@ public:
     FollowPath()
     {
         this->node = node;
+        using Graph = std::unordered_map<GraphPoint, std::vector<GraphPoint>>;
 
         ROS_INFO("Publishing Follow Path Node Data...");
         this->twistPublisher = node.advertise<geometry_msgs::Twist>("hexapod/teleop/twist", 1);
@@ -27,6 +30,9 @@ public:
         init_heading = getOrientation();
         position_error = 0.0;
         heading_error = 0.0;
+
+        target_speed = 0.04;
+        target_yaw = 0.15;
 
         twist_msg.linear.x = 0.0;
         twist_msg.linear.y = 0.0;
@@ -62,6 +68,7 @@ private:
     boost::mutex publish_mutex;
     std::string gait_type = "tetrapod";
     double max_speed, max_yaw;
+    double target_speed, target_yaw;
     double command_elapsed, command_start;
     geometry_msgs::Point curr_pos, init_pos;
     double curr_heading, init_heading;
@@ -77,8 +84,26 @@ private:
         // pathCommand(0.0, -1.0, 0.0, 3.0);
         // pathCommand(0.0, 0.0, 1.0, 3.0);
 
-        pathCommand(0.0, 0.0, 0.0, 0.0); // Dummy start command
-        pathWaypoint(0.2, 0.3, 0.5, 0.06, 0.15);
+        // pathWaypoint(-0.2, 0.3, -0.3, target_speed, target_yaw);
+
+        // Generate map of nodes to traverse
+        Graph graph = generateGraph();
+        
+        // Set start and goal points from nodes in map
+        GraphPoint start, goal;
+        start.x = init_pos.x;
+        start.y = init_pos.y;
+        goal.x = 0.2; // TODO: make sure this is a node in the graph
+        goal.y = 0.3;
+        
+        // Solve map using A star algorithm
+        std::set<GraphPoint> path = solveAStar(graph, start, goal);
+        
+        // Traverse waypoints of solved path
+        for (GraphPoint node : path)
+        {
+            pathWaypoint(node.x, node.y, 0.0, target_speed, target_yaw);
+        }
     }
 
     void pathWaypoint(
@@ -212,6 +237,149 @@ private:
         this->linkStateClient.call(linkStateMsg);
         // negative sign to match yaw convention
         return -linkStateMsg.response.link_state.pose.orientation.z;
+    }
+
+    struct GraphPoint
+    {
+        double x, y;
+
+        bool operator==(const GraphPoint& other) const
+        {
+            return (x == other.x && y == other.y);
+        }
+
+        bool operator!=(const GraphPoint& other) const
+        {
+            return (x != other.x || y != other.y);
+        }
+
+        bool operator<(const GraphPoint& other) const
+        {
+            return std::tie(x, y) < std::tie(other.x, other.y);
+        }
+    };
+
+    struct GraphPointHasher
+    {
+        std::size_t operator()(const GraphPoint& g) const
+        {
+            // Compute individual hash values for node.x and node.y
+            // and combine them using XOR and bit shifting:
+            return ((std::hash<double>()(g.x)
+                    ^ (std::hash<double>()(g.y) << 1)) >> 1);
+        }
+    };
+    
+    struct Graph
+    {
+        std::unordered_map<GraphPoint, std::vector<GraphPoint>, GraphPointHasher> points;
+
+        bool operator==(const Graph& other) const
+        {
+            return (points == other.points);
+        }
+    };
+    
+    Graph generateGraph()
+    {
+        Graph graph;
+        GraphPoint A{0.0, 0.0};
+        GraphPoint B{0.0, 1.0};
+        GraphPoint C{-2.0, -1.0};
+        GraphPoint D{2.0, 3.0};
+        GraphPoint E{1.0, -2.0};
+
+        // Add points and their neighbors
+        graph.points[A] = {B, E};
+        graph.points[B] = {D};
+        graph.points[C] = {A, E};
+        graph.points[D] = {A, E};
+        graph.points[E] = {C, D};
+
+        return graph;
+    }
+
+    template<typename T, typename priority_t>
+    struct PriorityQueue
+    {
+        typedef std::pair<priority_t, T> PQElement;
+        std::priority_queue<PQElement, std::vector<PQElement>,
+                        std::greater<PQElement>> elements;
+
+        inline bool empty() const
+        {
+            return elements.empty();
+        }
+
+        inline void put(T item, priority_t priority)
+        {
+            elements.emplace(priority, item);
+        }
+
+        T get()
+        {
+            T best_item = elements.top().second;
+            elements.pop();
+            return best_item;
+        }
+    };
+
+    std::set<GraphPoint> solveAStar(
+        Graph& graph,
+        const GraphPoint& start,
+        const GraphPoint& goal)
+    {
+        PriorityQueue<GraphPoint, double> open_set;
+        std::map<GraphPoint, GraphPoint> came_from;
+        std::map<GraphPoint, double> cost_so_far; // TODO: make default value infinity?
+        std::set<GraphPoint> path; // TODO: make sure these are all empty to start?
+        
+        open_set.put(start, 0.0);
+        came_from[start] = start;
+        cost_so_far[start] = 0.0;
+
+        while (!open_set.empty())
+        {
+            GraphPoint current = open_set.get();
+            if (current == goal)
+            {
+                while (current != start)
+                {
+                    path.insert(current);
+                    current = came_from[current];
+                }
+                return path;
+            }
+
+            std::vector<GraphPoint> neighbors = graph.points[current];
+            for (GraphPoint nbr : neighbors)
+            {
+                double new_cost = cost_so_far[current] + getCost(current, nbr);
+                if (cost_so_far.find(nbr) == cost_so_far.end() || new_cost < cost_so_far[nbr])
+                {
+                    cost_so_far[nbr] = new_cost;
+                    double priority = new_cost + heuristic(nbr, goal);
+                    open_set.put(nbr, priority);
+                    came_from[nbr] = current;
+                }
+            }
+        }
+
+        return path; // failure
+    }
+
+    inline double heuristic(
+        const GraphPoint& a,
+        const GraphPoint& b)
+    {
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    }
+
+    inline double getCost(
+        const GraphPoint& a,
+        const GraphPoint& b)
+    {
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
     }
 };
 
