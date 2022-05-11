8ccef50513bd6e7e17a506c556b185bc6de3211f
Kyle Erf <kyle.w.erf@gmail.com>
2022-05-10 23:22:02 -0400

    Added library of helper functions for gait_controller.cpp to clean up code

--- src/hexapod/hexapod_control/src/gait_controller.cpp
+++ src/hexapod/hexapod_control/src/gait_controller.cpp
@@ -3,10 +3,10 @@
 #include "hexapod_control/Pose.h"
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/Twist.h"
-#include "geometry_msgs/Point.h"
 #include "math.h"
 #include "boost/thread/mutex.hpp"
 #include "boost/thread/thread.hpp"
+#include "gait_controller_library.h"
 
 class GaitController
 {
@@ -58,13 +58,13 @@ public:
         publish_rate = 0.02; // 50Hz
         timer = node.createTimer(ros::Duration(publish_rate), boost::bind(&GaitController::updateAllLegs, this));
 
-        std::tie(dcw, dwi) = calcStepRadius(base_height);
-        cw_L1 = calcCw(base_height, base_radius, hip_angle_L1);
-        cw_L2 = calcCw(base_height, base_radius, hip_angle_L2);
-        cw_L3 = calcCw(base_height, base_radius, hip_angle_L3);
-        cw_R1 = calcCw(base_height, base_radius, hip_angle_R1);
-        cw_R2 = calcCw(base_height, base_radius, hip_angle_R2);
-        cw_R3 = calcCw(base_height, base_radius, hip_angle_R3);
+        std::tie(dcw, dwi) = calcStepRadius(base_height, coxa_length, femur_length, tibia_length);
+        cw_L1 = calcCw(base_height, base_radius, hip_angle_L1, dcw);
+        cw_L2 = calcCw(base_height, base_radius, hip_angle_L2, dcw);
+        cw_L3 = calcCw(base_height, base_radius, hip_angle_L3, dcw);
+        cw_R1 = calcCw(base_height, base_radius, hip_angle_R1, dcw);
+        cw_R2 = calcCw(base_height, base_radius, hip_angle_R2, dcw);
+        cw_R3 = calcCw(base_height, base_radius, hip_angle_R3, dcw);
         
         // Initialize legs to center of workspace
         ci_L1 = cw_L1;
@@ -364,7 +364,7 @@ private:
         theta = calcTheta(rmw, dwi);
         stride_time = abs(2.0*theta/yaw);
         diff_phase = Tc/stride_time;
-        std::tie(AEP, PEP) = calcAEPPEP(rmw, phi_w, dir, theta, base_height);
+        std::tie(AEP, PEP) = calcAEPPEP(cm, rmw, phi_w, dir, theta, base_height);
 
         // Not moving
         if (gait_mode == "Default")
@@ -550,156 +550,6 @@ private:
         std::tuple<geometry_msgs::Point, double, double, std::string> result(
             ci_new, support_phase, transfer_phase, stage);
         
-        return result;
-    }
-
-    geometry_msgs::Point calcCm(
-        const double& yaw_angle, const double& rm, const double& base_height)
-    {
-        // phi: current angle of the line connecting motion center and body center
-        // double phi = atan2(ci.y, ci.x);
-        double phi = yaw_angle - M_PI_2;
-
-        geometry_msgs::Point cm;
-        cm.x = rm*cos(phi);
-        cm.y = rm*sin(phi);
-        cm.z = -base_height;
-
-        return cm;
-    }
-
-    geometry_msgs::Point calcCw(
-        const double& base_height, const double& base_radius,
-        const double& hip_angle)
-    {
-        double foot_radius = base_radius + dcw;
-
-        geometry_msgs::Point cw;
-        cw.x = foot_radius*cos(hip_angle);
-        cw.y = foot_radius*sin(hip_angle);
-        cw.z = -base_height;
-
-        return cw;
-    }
-
-    double calcPhiI(
-        const geometry_msgs::Point& cm, const geometry_msgs::Point& ci)
-    {
-        // phi_i: current angle of the line connecting motion center and foot position
-        geometry_msgs::Point cm_to_ci;
-        cm_to_ci.x = ci.x - cm.x;
-        cm_to_ci.y = ci.y - cm.y;
-
-        return atan2(cm_to_ci.y, cm_to_ci.x);
-    }
-
-    std::tuple<double, double> calcPhiW(
-        const geometry_msgs::Point& cm, const geometry_msgs::Point& cw)
-    {
-        geometry_msgs::Point cm_to_cw;
-        cm_to_cw.x = cw.x - cm.x;
-        cm_to_cw.y = cw.y - cm.y;
-        double phi_w = atan2(cm_to_cw.y, cm_to_cw.x);
-        double rmw = sqrt(pow(cm_to_cw.x, 2.0) + pow(cm_to_cw.y, 2.0));
-        std::tuple<double, double> result(phi_w, rmw);
-        
-        return result;
-    }
-
-    double calcTheta(const double& rmw, const double& dwi)
-    {
-        // theta: angle from rotation center to edge of foot WS wrt cm
-        // https://mathworld.wolfram.com/Circle-CircleIntersection.html
-        double x = (pow(rmw, 2.0) - pow(dwi, 2.0) + pow(rmw, 2.0))/(2.0*rmw);
-        double y = sqrt(pow(rmw, 2.0) - pow(x, 2.0));
-        
-        return asin(y/rmw); // y = rmw*sin(theta)
-    }
-
-    std::tuple<geometry_msgs::Point, geometry_msgs::Point> calcAEPPEP(
-        const double& rmw, const double& phi_w, const double& dir,
-        const double& theta, const double& base_height)
-    {
-        geometry_msgs::Point AEP;
-        AEP.x = rmw*cos(phi_w - dir*theta) + cm.x;
-        AEP.y = rmw*sin(phi_w - dir*theta) + cm.y;
-        AEP.z = -base_height;
-
-        geometry_msgs::Point PEP;
-        PEP.x = rmw*cos(phi_w + dir*theta) + cm.x;
-        PEP.y = rmw*sin(phi_w + dir*theta) + cm.y;
-        PEP.z = -base_height;
-
-        std::tuple<geometry_msgs::Point, geometry_msgs::Point> result(AEP, PEP);
-
-        return result;
-    }
-
-    std::tuple<double, double, double> calcPhiAEPPEP(
-        const geometry_msgs::Point& cm, const geometry_msgs::Point& AEP,
-        const geometry_msgs::Point& PEP, const double& phi_i_old)
-    {
-        geometry_msgs::Point cm_to_AEP, cm_to_PEP;
-        cm_to_AEP.x = AEP.x - cm.x;
-        cm_to_AEP.y = AEP.y - cm.y;
-        cm_to_PEP.x = PEP.x - cm.x;
-        cm_to_PEP.y = PEP.y - cm.y;
-        
-        double phi_i, phi_AEP, phi_PEP;
-        phi_i = phi_i_old;
-        phi_AEP = atan2(cm_to_AEP.y, cm_to_AEP.x);
-        phi_PEP = atan2(cm_to_PEP.y, cm_to_PEP.x);
-
-        // Fix for L2 angle pi -> -pi crossover
-        if (phi_PEP > M_PI_2 && phi_AEP < -M_PI_2)
-        {
-            phi_AEP += 2.0*M_PI;
-            phi_i += (phi_i < 0.0) ? 2.0*M_PI : 0.0;
-        }
-        else if (phi_AEP > M_PI_2 && phi_PEP < -M_PI_2)
-        {
-            phi_PEP += 2.0*M_PI;
-            phi_i += (phi_i < 0.0) ? 2.0*M_PI : 0.0;
-        }
-        
-        std::tuple<double, double, double> result(phi_i, phi_AEP, phi_PEP);
-
-        return result;
-    }
-
-
-    std::tuple<double, double> calcStepRadius(const double& base_height)
-    {
-        double beta;
-        beta = asin(base_height/(femur_length + tibia_length)); // full stretch leg angle
-        dwi = (femur_length + tibia_length)*cos(beta)/2.0; // workspace radius
-        dcw = coxa_length + dwi; // radius from hip to workspace center
-
-        // Limit dwi to use smaller workspace radius without changing cw workspace center
-        dwi *= 0.5;
-
-        std::tuple<double, double> result(dcw, dwi);
-
-        return result;
-    }
-
-    double LPF1(const double& goal, const double& z_prev, const double& rate)
-    {
-        // 1st order low-pass filter
-        // https://dsp.stackexchange.com/questions/60277/is-the-typical-implementation-of-low-pass-filter-in-c-code-actually-not-a-typica
-        // rate must be between 0.0 and 1.0. as rate approaches 0, the cutoff of the filter decreases
-        double z_next = (1 - rate) * z_prev + rate * goal; // "simplistic" low pass filter
-
-        return z_next;
-    }
-
-    double mapRange(const double& inValue,
-                    const double& minInRange, const double& maxInRange,
-                    const double& minOutRange, const double& maxOutRange)
-    {
-        double x = (inValue - minInRange) / (maxInRange - minInRange);
-        double result = minOutRange + (maxOutRange - minOutRange) * x;
-
         return result;
     }
 
---
+++ src/hexapod/hexapod_control/src/gait_controller_library.h
@@ -0,0 +1,35 @@
+#include "geometry_msgs/Point.h"
+
+geometry_msgs::Point calcCm(
+    const double& yaw_angle, const double& rm, const double& base_height);
+
+geometry_msgs::Point calcCw(
+    const double& base_height, const double& base_radius,
+    const double& hip_angle, const double& dcw);
+
+double calcPhiI(
+    const geometry_msgs::Point& cm, const geometry_msgs::Point& ci);
+
+std::tuple<double, double> calcPhiW(
+    const geometry_msgs::Point& cm, const geometry_msgs::Point& cw);
+
+double calcTheta(const double& rmw, const double& dwi);
+
+std::tuple<geometry_msgs::Point, geometry_msgs::Point> calcAEPPEP(
+    const geometry_msgs::Point& cm, 
+    const double& rmw, const double& phi_w, const double& dir,
+    const double& theta, const double& base_height);
+
+std::tuple<double, double, double> calcPhiAEPPEP(
+    const geometry_msgs::Point& cm, const geometry_msgs::Point& AEP,
+    const geometry_msgs::Point& PEP, const double& phi_i_old);
+
+double LPF1(const double& goal, const double& z_prev, const double& rate);
+
+double mapRange(const double& inValue,
+                const double& minInRange, const double& maxInRange,
+                const double& minOutRange, const double& maxOutRange);
+
+std::tuple<double, double> calcStepRadius(
+    const double& base_height, const double& coxa_length,
+    const double& femur_length, const double& tibia_length);
